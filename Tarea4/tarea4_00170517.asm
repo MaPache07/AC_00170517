07e30d3f91f9dc6f2ddadeee3975ed3f54f10596
MaPache07 <00170517@uca.edu.sv>
2020-06-13 23:22:35 -0600

    Se han agregado los mensajes de cada nota

--- Tarea4/tarea4_00170517.asm
+++ Tarea4/tarea4_00170517.asm
@@ -2,25 +2,83 @@ org 	100h
 
 section .text
 
-	xor 	si, si 	;lo mimso que: mov si, 0000h
+        call    texto
+        call    phrase
+
+	    xor 	si, si 	;lo mimso que: mov si, 0000h
 lupi:	call 	kb
-	cmp 	al, "$"
-	je	mostrar
-	mov	[300h+si], al ; CS:0300h en adelante
-	inc 	si
-	jmp 	lupi
+        cmp 	al, "$"
+        je	    mostrar
+        mov	    [300h+si], al ; CS:0300h en adelante
+        inc 	si
+        jmp 	lupi
+        int     20h
 
-mostrar:mov	byte [300h+si], "$"
-	call 	w_strng
+texto:	mov 	ah, 00h
+        mov	al, 03h
+        int 	10h
+        ret
 
-	call 	kb	; solo detenemos la ejecución
-	int 	20h
+mostrar:call    texto
+        mov	    byte [300h+si], "$"
+	    call 	w_strng
 
-kb: 	mov	ah, 7h
-	int 	21h
-	ret
+        call 	kb	; solo detenemos la ejecución
+        int 	20h
 
-w_strng:mov	ah, 09h
-	mov 	dx, 300h
-	int 	21h
-	ret
+kb: 	mov	    ah, 7h
+        int 	21h
+        ret
+
+w_strng:mov	    ah, 09h
+        mov 	dx, 300h
+        int 	21h
+        ret
+
+w_char:	mov 	ah, 09h
+        mov 	al, cl
+        mov 	bh, 0h
+        mov 	bl, 00001111b
+        mov 	cx, 1h
+        int 	10h
+        ret
+
+cursr:  mov 	ah, 02h
+        mov 	dx, di 
+        mov 	dh, 0d
+        mov 	bh, 0h
+        int 	10h
+        ret
+
+phrase:	mov 	di, 0d
+lupis:	mov 	cl, [msg+di]
+        call    cursr
+        call 	w_char
+        inc	    di
+        cmp 	di, len
+        jb	    lupis
+        ret
+
+section .data
+msg 	db 	    "Ingrese su numero de carnet (presione $ para ingresar).  "
+len 	equ	    $-msg
+msg1    db      "Solo necesito el 0"
+len1    equ     $-msg1
+msg2    db      "Aun se pasa"
+len2    equ     $-msg2
+msg3    db      "Hay salud"
+len3    equ     $-msg3
+msg4    db      "Me recupero"
+len4    equ     $-msg4
+msg5    db      "En el segundo"
+len5    equ     $-msg5
+msg6    db      "Peor es nada"
+len6    equ     $-msg6
+msg7    db      "Muy bien"
+len7    equ     $-msg7
+msg8    db      "Colocho"
+len8    equ     $-msg8
+msg9    db      "Siempre me esfuerzo"
+len9    equ     $-msg9
+msg10   db      "Perfecto solo Dios"
+len10   equ     $-msg10
